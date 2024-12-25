ca2028ad8ffaa7e8d3bb4a9cbdd854af19eeb0e9
Christopher <rxchris94@gmail.com>
2024-12-25 19:06:22 +0800

    feat(routes): Set root path to "curricula#index"

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
+  root "curricula#index"
 
   resources :curricula, only: %i[index]
 end

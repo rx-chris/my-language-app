705870b7068957bc1cafe516c0ca69d863ebcef2
Christopher <rxchris94@gmail.com>
2024-12-27 13:29:56 +0800

    feat(layout): Created main layout component that includes the sidebar and <main> element

---
+++ app/frontend/layouts/Layout.vue
@@ -0,0 +1,15 @@
+<template>
+    <SidebarProvider :defaultOpen="true">
+        <AppSidebar />
+        <main style="width: calc(100vw - var(--sidebar-width-icon) - 2.5rem);">
+            <slot></slot>
+        </main>
+    </SidebarProvider>
+</template>
+
+<script setup lang="ts">
+import AppSidebar from '@/components/AppSidebar.vue';
+import { SidebarProvider } from '@/components/ui/sidebar';
+</script>
+
+<style scoped></style>

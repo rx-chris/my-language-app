810a2c7b1c13b491fc894ec8c593544070dd9667
Christopher <rxchris94@gmail.com>
2024-12-28 03:18:23 +0800

    feat(component): Created default AppBreadCrumb component for the quick navigation

---
+++ app/frontend/components/AppBreadCrumb.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb'
+import { Link } from '@inertiajs/vue3'
+import { cn } from '@/lib/utils'
+
+interface BreadCrumbProps {
+    title: string,
+    href?: string,
+    maxChar?: null,
+}
+
+interface Props {
+    breadCrumbProps: BreadCrumbProps[]
+}
+
+const { breadCrumbProps } = defineProps<Props>()
+
+</script>
+
+<template>
+    <Breadcrumb :class="cn('mb-3', $attrs.class ?? '')">
+        <BreadcrumbList>
+            <template v-for="(prop, index) in breadCrumbProps">
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                    <Link v-if="index < (breadCrumbProps.length - 1)" :href="prop.href!"
+                        class="transition-colors hover:text-foreground">
+                    {{ prop.title }}
+                    </Link>
+                    <BreadcrumbPage v-else class="font-semibold">
+                        {{ prop.title }}
+                    </BreadcrumbPage>
+                </BreadcrumbItem>
+            </template>
+        </BreadcrumbList>
+    </Breadcrumb>
+</template>

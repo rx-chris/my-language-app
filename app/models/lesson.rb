d2b04edfc35cc31bb31acd8179f11ec04d6375ec
Christopher <rxchris94@gmail.com>
2024-12-28 03:16:08 +0800

    feat(model): Added getter methods to Lesson model for filtered cards and aggregated values

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -6,4 +6,31 @@ class Lesson < ApplicationRecord
 
   # validations
   validates :title, :description, :order, presence: true
+
+  # getter methods for filtered cards
+  def completed_cards
+    cards.where.not(correct: nil)
+  end
+
+  def incomplete_cards
+    cards.where(correct: nil)
+  end
+
+  def correct_cards
+    cards.where(correct: true)
+  end
+
+  def wrong_cards
+    cards.where(correct: false)
+  end
+
+  # getter methods for aggregated card properties
+  def progress
+    cards.empty? ? 0 : completed_cards.length.to_f / cards.length
+  end
+
+  def score
+    correct = correct_cards
+    correct.empty? ? 0 : correct.length.to_f / completed_cards
+  end
 end

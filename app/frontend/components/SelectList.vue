44cd79cb76dca583399fb4c37aefe32b2c6055f6
Christopher <rxchris94@gmail.com>
2024-12-29 23:55:18 +0800

    feat(component): Created input component to render a select list

---
+++ app/frontend/components/SelectList.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select'
+
+interface Props {
+    listItems: {
+        value: string,
+        label: string
+    }[],
+    placeholder?: string
+}
+
+const { listItems, placeholder = "Select a value" } = defineProps<Props>()
+
+const selectedValue = defineModel<string>();
+
+</script>
+<template>
+    <Select v-model="selectedValue">
+        <SelectTrigger>
+            <SelectValue :placeholder="placeholder" />
+        </SelectTrigger>
+        <SelectContent>
+            <template v-for="item in listItems" :key="item.value">
+                <SelectItem :value="item.value.toString()">
+                    {{ item.label }}
+                </SelectItem>
+            </template>
+        </SelectContent>
+    </Select>
+</template>
+
+
+<style scoped></style>

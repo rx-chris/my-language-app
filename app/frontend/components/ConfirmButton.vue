0aad1bfbc5ce2b2b22639fa162316f007410efa0
Christopher <rxchris94@gmail.com>
2024-12-30 02:39:47 +0800

    feat(component): Created button component with a confirmation modal

---
+++ app/frontend/components/ConfirmButton.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+import { Button } from '@/components/ui/button'
+
+defineOptions({ inheritAttrs: false })
+
+const emit = defineEmits(['cancel', 'continue'])
+
+</script>
+<template>
+    <AlertDialog>
+        <AlertDialogTrigger as-child>
+            <Button v-bind="{ ...$attrs }">
+                <slot></slot>
+            </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+            <AlertDialogHeader>
+                <AlertDialogTitle>
+                    <slot name="title">Are you absolutely sure?</slot>
+                </AlertDialogTitle>
+                <AlertDialogDescription>
+                    <slot name="description">
+                        This action is permanent and cannot be undone.
+                    </slot>
+                </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+                <AlertDialogCancel @click="$emit('cancel')">
+                    <slot name="cancel">Cancel</slot>
+                </AlertDialogCancel>
+                <AlertDialogAction @click="$emit('continue')">
+                    <slot name="continue">Continue</slot>
+                </AlertDialogAction>
+            </AlertDialogFooter>
+        </AlertDialogContent>
+    </AlertDialog>
+</template>
+
+
+<style scoped></style>

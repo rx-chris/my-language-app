f9695642eb820ce7a94a5ab0324464c5cedb6dfe
Christopher <rxchris94@gmail.com>
2025-01-01 23:03:35 +0800

    feat(services): Added methods to generate cards in OpenAI service

--- app/services/json_schema.rb
+++ app/services/json_schema.rb
@@ -24,4 +24,67 @@ module Json_Schema
       }
     }
   }
+
+  # Dynamically generate a cards schema based on the content_type and input_type
+  def self.cards_schema(args = { content_type: "text_content", input_type: "text" })
+    args => { content_type:, input_type:}
+
+    properties = {
+      instruction: {
+        description: "card instructions",
+        type: "string"
+      },
+      model_answer: {
+        description: "The correct answer to the card instructions",
+        type: "string"
+      },
+      pronunciation: {
+        description: "The pronunciation of model_answer",
+        type: "string"
+      }
+    }
+    content = {
+      "text_content"=>{
+        description: "a phrase or sentence for the learning card",
+        type: "string"
+      },
+      "image_content"=>{
+        description: "an image caption that can be used to generate an image",
+        type: "string"
+      },
+      "audio_content"=>{
+        description: "A phrase or sentence that will be converted to a audio file",
+        type: "string"
+      }
+    }
+
+    properties[content_type.to_sym] = content[content_type]
+
+
+    properties[:mcq_options] = {
+      description: "a list of answers that the user can choose from",
+      type: "array",
+      items: {
+        type: "string"
+      }
+    } if input_type == "mcq"
+
+    # final schema
+    {
+      name: "cards_schema",
+      schema: {
+        type: "object",
+        properties: {
+          cards: {
+            description: "list of cards",
+            type: "array",
+            items: {
+              type: "object",
+              properties:
+            }
+          }
+        }
+      }
+    }
+  end
 end
--- app/services/open_ai_service.rb
+++ app/services/open_ai_service.rb
@@ -46,4 +46,99 @@ class OpenAiService
 
     "#{instructions} #{context}"
   end
+
+  # ----------------------------------------
+  # Cards Generation
+  # ----------------------------------------
+  def generate_cards(request, unprocessed: false)
+    response = @client.chat(
+      parameters: {
+        model: "gpt-4o-mini",
+        messages: [ { role: "user", content: cards_message_content(request) } ],
+        temperature: 0.7,
+        response_format: {
+          type: "json_schema",
+          json_schema: Json_Schema.cards_schema(request)
+        }
+      }
+    )
+    unprocessed ? response : response.dig("choices", 0, "message", "content")
+  end
+
+  def cards_message_content(req)
+    contents = {
+      "text_content" => "text_content: Provide a phrase or sentence in English or #{req[:language]}.",
+      "audio_content" => "audio_content: Provide a phrase or sentence in #{req[:language]} that will represent an audio file.",
+      "image_content" => "image_content: Provide an image caption to be used for image generation. Be as descriptive as possible"
+    }
+    in_language = req[:content_type] == "image_content" ? "in #{req[:language]} " : ""
+
+    message = "I am designing a #{req[:language]} lesson with the following title and description:\n"
+    message += "- title: \"#{req[:lesson_title]}\"\n"
+    message += "- description: \"#{req[:lesson_description]}\"\n"
+    message += "Recommend me #{req[:card_count]} #{req[:language]} learning cards for the above lesson as a JSON array that includes the following details for each card:\n"
+
+    card_details = [ "instruction: Card instructions that says \"#{req[:instruction]}\"" ]
+    card_details << contents[req[:content_type]]
+    card_details << "model_answer: Provide the correct answer #{in_language}based on the 'instruction' and '#{req[:content_type]}'"
+    card_details << "pronunciation: Provide the pronunciation of the 'model_answer' only if it is in #{req[:language]} or else this should be 'null'"
+
+    if req[:input_type] == "mcq"
+      card_details << "mcq_options: Provide a list of 4 answers to choose whereby 1 of the is the 'model_answer' and the rest are incorrect answers."
+    end
+
+    message += card_details.map.with_index { |content, index| "#{index + 1}. #{content}" }.join("\n")
+    message
+  end
+
+  def test_cards_generation
+    generate_cards(mock_cards_request)
+  end
+
+  def test_audio_cards_generation
+    generate_cards(mock_audio_cards_request)
+  end
+
+  def test_image_cards_generation
+    generate_cards(mock_image_cards_request)
+  end
+
+  def mock_cards_request
+    {
+      learning_objective: "I will be going on holiday to Japan for 7 days I would like to learn common Japanese phrases that can help me communicate effectively with the locals.",
+      language: "Japanese",
+      lesson_title: "Ordering Food",
+      lesson_description: "Learn how to order food at a restaurant. Key phrases include 'Menu wo misete kudasai' (Please show me the menu), 'Kore wo kudasai' (I'll have this), and 'Oishii desu' (It's delicious). Explore common Japanese dishes and how to express dietary preferences.",
+      card_count: 10,
+      instruction: "Translate the following phrase or sentence",
+      content_type: "text_content",
+      input_type: "text"
+    }
+  end
+
+  def mock_audio_cards_request
+    {
+      learning_objective: "I will be going on holiday to Japan for 7 days I would like to learn common Japanese phrases that can help me communicate effectively with the locals.",
+      language: "Japanese",
+      lesson_title: "Ordering Food",
+      lesson_description: "Learn how to order food at a restaurant. Key phrases include 'Menu wo misete kudasai' (Please show me the menu), 'Kore wo kudasai' (I'll have this), and 'Oishii desu' (It's delicious). Explore common Japanese dishes and how to express dietary preferences.",
+      card_count: 1,
+      instruction: "Listen to the audio and select the correct answer.",
+      content_type: "audio_content",
+      input_type: "mcq"
+    }
+  end
+
+  def mock_image_cards_request
+    {
+      learning_objective: "I will be going on holiday to Japan for 7 days I would like to learn common Japanese phrases that can help me communicate effectively with the locals.",
+      language: "Japanese",
+      lesson_title: "Ordering Food",
+      lesson_description: "Learn how to order food at a restaurant. Key phrases include 'Menu wo misete kudasai' (Please show me the menu), 'Kore wo kudasai' (I'll have this), and 'Oishii desu' (It's delicious). Explore common Japanese dishes and how to express dietary preferences.",
+      card_count: 1,
+      instruction: "Select the answer that best describes the image",
+      content_type: "image_content",
+      input_type: "mcq"
+    }
+  end
 end

6f37cd1242afd7d1579239498b9f50e51d413ba5
Christopher <rxchris94@gmail.com>
2025-01-03 02:18:03 +0800

    feat(job): Added background job to create cards

---
+++ app/jobs/create_cards_job.rb
@@ -0,0 +1,43 @@
+class CreateCardsJob < ApplicationJob
+  queue_as :default
+
+  def perform(selected_blueprint, lesson)
+    # initialise OpenAI service
+    service = OpenAiService.new
+
+    # generate card data from OpenAI
+    response = service.generate_cards({
+      language: lesson.curriculum.language.name,
+      lesson_title: lesson.title,
+      lesson_description: lesson.description,
+      card_count: selected_blueprint.count,
+      instruction: selected_blueprint.blueprint.instruction,
+      content_type: selected_blueprint.blueprint.content_type,
+      input_type: selected_blueprint.blueprint.input_type
+    })
+
+    # create cards
+    response[:cards].each do |data|
+      card = Card.new(
+        blueprint: selected_blueprint.blueprint,
+        lesson:,
+        model_answer: data[:model_answer]
+      )
+      case selected_blueprint.blueprint.content_type
+      when "text_content"
+        card.text_content = data[:text_content]
+      when "audio_content"
+        file_path = service.generate_audio(data[:audio_content])
+        file = File.open(file_path)
+        filename = File.basename(file_path)
+        card.audio_content.attach(io: file, filename:, content_type: "audio/mpeg")
+      when "image_content"
+        url = service.generate_image(data[:image_content])
+        file = URI.parse(url).open
+        filename = "#{DateTime.now.strftime('%Q').to_i}_image.png"
+        card.image_content.attach(io: file, filename:, content_type: "image/png")
+      end
+      card.save!
+    end
+  end
+end

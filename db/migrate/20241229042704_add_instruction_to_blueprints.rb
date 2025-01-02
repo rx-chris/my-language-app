da8e28652d9ebb145388faab35e6ef5d669937fd
Christopher <rxchris94@gmail.com>
2025-01-02 23:19:24 +0800

    fix(migration): Fixed `change_column_default` to inclue :from and :to arguments to allow for rollbacks

--- db/migrate/20241229042704_add_instruction_to_blueprints.rb
+++ db/migrate/20241229042704_add_instruction_to_blueprints.rb
@@ -1,6 +1,6 @@
 class AddInstructionToBlueprints < ActiveRecord::Migration[8.0]
   def change
     add_column :blueprints, :instruction, :text, null: false, default: 'Enter instructions here'
-    change_column_default :blueprints, :instruction, nil
+    change_column_default :blueprints, :instruction, from: 'Enter instructions here', to: nil
   end
 end

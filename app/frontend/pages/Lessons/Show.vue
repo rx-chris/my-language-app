3f27cd47a7b0acbc201f7c3cf0acbdd5ab7c6089
Christopher <rxchris94@gmail.com>
2025-01-04 18:32:50 +0800

    feat(page): Added mode toggle on lesson show page

--- app/frontend/pages/Lessons/Show.vue
+++ app/frontend/pages/Lessons/Show.vue
@@ -4,9 +4,12 @@ import Container from '@/layouts/Container.vue';
 import { Link } from '@inertiajs/vue3';
 import { Card, CardContent, CardDescription, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button'
+import { Label } from '@/components/ui/label';
 import ProgressRing from '@/components/ProgressRing.vue';
 import LessonCardItem from '@/components/LessonCardItem.vue';
 import AppBreadCrumb from '@/components/AppBreadCrumb.vue';
+import { Switch } from '@/components/ui/switch';
+import { ref } from 'vue';
 
 defineOptions({ layout: [Layout, Container] })
 
@@ -25,6 +28,8 @@ const breadCrumbProps = [
         title: lesson.title,
     }
 ]
+
+const isLearningMode = ref(true)
 </script>
 
 <template>
@@ -51,11 +56,15 @@ const breadCrumbProps = [
             </CardContent>
         </Card>
 
-        <h2 class="mb-2 mt-4 text-xl font-semibold">Cards:</h2>
         <!-- Cards -->
+        <div class="flex items-center gap-4 mb-2 mt-6">
+            <h2 class="text-xl font-semibold grow">Cards:</h2>
+            <Label for="mode">{{ isLearningMode ? 'Learning' : 'Quiz' }} Mode</Label>
+            <Switch id="mode" v-model:checked="isLearningMode" />
+        </div>
         <div class="flex flex-col gap-1.5 mt-3">
             <template v-for="card in lesson.cards" :key="card.id">
-                <Link :href="card.learning_url">
+                <Link :href="isLearningMode ? card.learning_url : card.test_url">
                 <LessonCardItem :card="card" />
                 </Link>
             </template>

e8865e5f837d3bbdc1437ae0ec284db73d9962d9
Christopher <rxchris94@gmail.com>
2025-01-05 19:27:42 +0800

    fix(component): fix color and alignment styles in ProgressRing component

--- app/frontend/components/ProgressRing.vue
+++ app/frontend/components/ProgressRing.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
+import { templateRef } from '@vueuse/core';
 import Chart from 'chart.js/auto'
-import { computed, onMounted, ref } from 'vue';
-import type { Ref } from 'vue';
+import { computed, onMounted } from 'vue';
 
 interface Props {
     value: number
@@ -13,8 +13,7 @@ const { value = 0, size = '100%', options = {} } = defineProps<Props>()
 
 const progressValue = computed(() => `${(value * 100).toFixed(1)}%`)
 
-type K = HTMLCanvasElement | null
-const ctx: Ref<K, K> = ref(null);
+const ctx = templateRef('ctx');
 
 onMounted(() => {
     new Chart(ctx.value!, {
@@ -22,15 +21,32 @@ onMounted(() => {
         data: {
             datasets: [{
                 data: [value, 1.0 - value],
+                backgroundColor: [
+                    'rgb(104, 222, 136)',
+                    'rgb(225, 225, 225)',
+                ],
                 ...options
             }]
         },
+        options: {
+            aspectRatio: 1,
+            plugins: {
+                legend: {
+                    display: false
+                },
+                tooltip: {
+                    callbacks: {
+                        label: (item) => `${(Number(item.formattedValue) * 100).toFixed(1)}%`
+                    }
+                }
+            }
+        }
     });
 })
 </script>
 
 <template>
-    <div :style="`width: ${size}; height: ${size};`" class="relative flex justify-center align-middle">
+    <div :style="`width: ${size}; height: ${size};`" class="relative flex justify-center items-center">
         <canvas ref="ctx"></canvas>
         <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">{{ progressValue }}</div>
     </div>
